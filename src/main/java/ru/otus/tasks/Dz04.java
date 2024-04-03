924220c7cd8c5bace6b700a2f30b19e6c167fb8c
karakyan_om <olenyshka4@gmail.com>
2024-04-03 17:47:51 +0300

    внесены правки

--- src/main/java/ru/otus/tasks/Dz04.java
+++ src/main/java/ru/otus/tasks/Dz04.java
@@ -10,14 +10,15 @@ public class Dz04 {
         System.out.println();
 
         int[] values1 = {1, 3, 7, 4, 9, 7, 6, 9};
+        int[] values2 = values1.clone();
         printArray(values1);
         System.out.println();
 
         fillCell(n, values1);
         System.out.println();
 
-        int[] values2 = {1, 3, 7, 4, 9, 7, 6, 9};
-        addArrayValue(n, values2);
+        //int[] values2 = {1, 3, 7, 4, 9, 7, 6, 9};
+        increaseArrayElementsByN(n, values2);
         System.out.println();
 
         compareHalfArray(values2);
@@ -33,9 +34,9 @@ public class Dz04 {
 
     public static void printArray(int[] array) {
         int sum = 0;
-        for (int i= 0; i < array.length - 1; i++) {
-            if (array[i]>5) {
-                sum += array[i];
+        for (int j : array) {   //(int i= 0; i < array.length; i++)
+            if (j > 5) {
+                sum += j;
             }
         }
         System.out.println(sum);
@@ -48,7 +49,7 @@ public class Dz04 {
         }
     }
 
-    public static void addArrayValue(int n, int[] array){
+    public static void increaseArrayElementsByN(int n, int[] array){       //вместо addArrayValue
         for (int i = 0; i < array.length; i++) {
             array[i] += n;
             System.out.print(" " + array[i]);
@@ -58,16 +59,21 @@ public class Dz04 {
     public static void compareHalfArray(int[] array){
         int sum1 = 0;
         int sum2 = 0;
-        for (int i = 0; i < array.length/2; i++){
-            sum1 += array[i];
-        }
-        for (int i = array.length/2; i < array.length; i++) {
-            sum2 += array[i];
+        for (int i = 0; i < array.length; i++){
+            if (i < array.length/2) {
+                sum1 += array[i];
+            } else {
+                sum2 += array[i];
+            }
+//        for (int i = array.length/2; i < array.length; i++) {
+//            sum2 += array[i];
         }
         if (sum1 > sum2) {
             System.out.println("Левая половина больше, чем правая: " + sum1 + " > " + sum2);
-        } else {
+        } else if (sum1 < sum2){
             System.out.println("Правая половина больше, чем левая: " + sum2 + " < " + sum1);
+        } else {
+            System.out.println("Половины равны: " + sum1 + " = " + sum2);   //А если половины равны?
         }
 
     }
